b931dcc62928c1e985b8a48361d8d6653a18bc2f
alex krivospitsky <alex@krivospitsky.ru>
2013-10-12 17:46:46 +0400

    commit

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,5 @@
 class AuthController < ApplicationController
-	http_basic_authenticate_with name: ADMIN_NAME password: ADMIN_PASSWORD
+	http_basic_authenticate_with name: ADMIN_NAME, password: ADMIN_PASSWORD
 	def index
 		session[:is_admin?]=true
 		redirect_to categories_path
