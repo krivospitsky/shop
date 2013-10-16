7ab62e969fd692ad13cfdf3095022505b40fdcf8
alex krivospitsky <alex@krivospitsky.ru>
2013-10-16 17:10:48 +0400

    commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 #coding:utf-8
 class UserMailer < ActionMailer::Base
+  default :delivery_method => 'sendmail'
   def order_confirmation(order)
     @order=order
     mail(to:order.email, subject:"Подтверждение заказа", from:"order@cargadget.ru")
